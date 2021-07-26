9cf5d681f0cb3675b53fe2573f312906c3303552
Muhammad Usman <usman@alliedblock.com>
2021-07-26 11:12:39 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        static void Main(string[] args)
+        {
+            Process process = null;
+
+            var task = Task.Run(() =>
+            {
+                process = Test().Result;
+            });
+
+            task.Wait();
+            if (process != null)
+            {
+                Console.WriteLine(process.ProcessName);
+                System.Threading.Thread.Sleep(5000);
+                Console.WriteLine("Suspending for 90 seconds");
+                SuspendProcess(process);
+                System.Threading.Thread.Sleep(90000);
+                Console.WriteLine("Resuming");
+                ResumeProcess(process);
+
+                while (!process.HasExited)
+                {
+                    Console.WriteLine("Waiting");
+                    System.Threading.Thread.Sleep(5000);
+                }
+
+                Console.WriteLine(process.ExitCode);
+            }
+        }
+
+        private static Task<Process> Test()
+        {
+            var process = new Process();
+            process.StartInfo.RedirectStandardOutput = false;
+            process.StartInfo.RedirectStandardError = false;
+            process.StartInfo.FileName = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\ffmpeg.exe";
+            process.StartInfo.Arguments = @"-i outputFile.mp4 -vcodec libx265 -crf 28 outputCompressed.mp4";
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            return Task.FromResult(process);
+        }
+
+        private static void SuspendProcess(Process process)
+        {
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        public static void ResumeProcess(Process process)
+        {
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+    }
+}
